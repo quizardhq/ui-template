0ed142e708dda1df2300db65876e0a14322cf538
CeoFred <johnsonmessilo19@gmail.com>
2023-05-22 21:01:01 +0100

    chore: remove unnecessary data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
-import ComingSoon from '@/components/ui/organisms/ComingSoon';
+import HomePage from '@/components/ui/organisms/Home';
 import HomeLayout from '@/layouts/HomeLayout';
 import MetaTag from '@/components/ui/atoms/MetaTag';
 
 export default function Home() {
   return (
     <HomeLayout>
-      <MetaTag
-        title="Quizard - Engage and Interact with your Audience"
-        description="Discover the fun and engaging world of personalized learning with Quizard, the open-source quiz platform. Create, share and play custom quizzes with ease. Join our community today!"
-        url="/"
-      />
-      <ComingSoon />
+      <MetaTag title="Home" description="" url="/" />
+      <HomePage />
     </HomeLayout>
   );
 }
